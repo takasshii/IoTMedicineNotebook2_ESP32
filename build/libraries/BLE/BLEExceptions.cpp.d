ad19df5e9ce1d965489df1897708126c8eab3de2
takashi <takashiho0913@gmail.com>
2023-04-25 15:47:21 +0900

    [remove]

--- build/libraries/BLE/BLEExceptions.cpp.d
+++
@@ -1,2 +0,0 @@
-/Users/takashi/Documents/Arduino/IoT_medicine_notebook_2/build/libraries/BLE/BLEExceptions.cpp.o: \
- /Users/takashi/Library/Arduino15/packages/esp32/hardware/esp32/1.0.6/libraries/BLE/src/BLEExceptions.cpp
--- build/sketch/README.md
+++
@@ -1,2 +0,0 @@
-#line 1 "/Users/takashi/Documents/Arduino/IoT_medicine_notebook_2/README.md"
-# IoTMedicineNotebook2_ESP32
